ce743785842dc9addaefd8df517fe16c5ed46433
Yannis Flet-Berliac <yannisfbe@gmail.com>
2017-02-05 01:53:13 +0100

    need to check the loss for this one

--- model/model.py
+++ model/model.py
@@ -1,5 +1,5 @@
 """
-This module constructs model_fn function that will be fed into the Estimator.
+This module constructs model_fn that will be fed into the Estimator.
 """
 from __future__ import absolute_import
 from __future__ import print_function
@@ -11,6 +11,7 @@ import tensorflow as tf
 def model_fn(features, targets, mode, params, scope=None):
     embedding_size = params['embedding_size']
     vocab_size = params['vocab_size']
+    max_story_length = params['max_story_length']
     max_story_char_length = params['max_story_char_length']
     hidden_units = params['hidden_units']
     debug = params['debug']
@@ -28,17 +29,37 @@ def model_fn(features, targets, mode, params, scope=None):
                                      shape=[vocab_size, 1])
         embedding_params_masked = embedding_params * embedding_mask  # 38 * 10
         story_embedding = tf.nn.embedding_lookup(embedding_params_masked, story)  # ? * 1 * 311 * 10
+        # Indices of the spaces between words
+        indices_word = get_word_indices(story, batch_size, max_story_char_length)
+        indices_per_batch = tf.shape(indices_word)
+        offset = tf.expand_dims(tf.range(0, batch_size) * max_story_char_length, 1)
+        flattened_indices = tf.reshape(indices_word + offset, [-1])
 
         # Recurrence
         story_length = get_story_char_length(story_embedding)
         embedded_input = tf.reshape(story_embedding, [batch_size, max_story_char_length, embedding_size])
 
-        cell = tf.nn.rnn_cell.GRUCell(hidden_units)
-        initial_state = cell.zero_state(batch_size, tf.float32)
+        cell_1 = tf.nn.rnn_cell.GRUCell(hidden_units, activation=tf.nn.tanh)
+        cell_2 = tf.nn.rnn_cell.GRUCell(hidden_units)
+        initial_state_1 = cell_1.zero_state(batch_size, tf.float32)
+        initial_state_2 = cell_2.zero_state(batch_size, tf.float32)
 
-        outputs_rnn1, last_state = tf.nn.dynamic_rnn(cell, embedded_input,
+        with tf.variable_scope('cell_1'):
+            outputs_1, _ = tf.nn.dynamic_rnn(cell_1, embedded_input,              # ? * 311 * 100
                                                      sequence_length=story_length,
-                                                     initial_state=initial_state)
+                                                     initial_state=initial_state_1)
+
+        flattened_output_1 = tf.reshape(outputs_1, tf.concat(0, [[-1], tf.shape(outputs_1)[2:]]))
+
+        selected_rows = tf.nn.embedding_lookup(flattened_output_1, flattened_indices)
+
+        input_rnn2 = tf.reshape(selected_rows,
+                                tf.concat(0, [tf.pack([batch_size, indices_per_batch[-1]]), tf.shape(outputs_1)[2:]]))
+        input_rnn2.set_shape([None, None, hidden_units])
+
+        with tf.variable_scope('cell_2'):
+            outputs_2, last_state = tf.nn.dynamic_rnn(cell_2, input_rnn2,
+                                                      initial_state=initial_state_2)
 
         # Output
         output = get_output(last_state,
@@ -53,7 +74,6 @@ def model_fn(features, targets, mode, params, scope=None):
 
         if debug:
             tf.contrib.layers.summarize_tensor(story_length, 'story_length')
-            tf.contrib.layers.summarize_tensor(outputs_rnn1, 'outputs_rnn1')
             tf.contrib.layers.summarize_tensor(last_state, 'last_state')
             tf.contrib.layers.summarize_tensor(output, 'output')
             tf.contrib.layers.summarize_variables()
@@ -72,6 +92,17 @@ def get_story_char_length(sequence, scope=None):
         tmp = tf.cast(tf.reduce_sum(used, reduction_indices=[-1]), tf.int32)
         length = tf.reduce_max(tmp, reduction_indices=[-1])
         return length
+
+
+def get_word_indices(story, batch_size, max_story_char_length, scope=None):
+    with tf.variable_scope(scope, 'WordIndices'):
+        spaces = tf.constant(1, dtype=tf.int64)
+        story = tf.reshape(story, [batch_size, max_story_char_length])
+        where = tf.equal(story, spaces)
+        tmp = tf.where(where)
+        indices = tf.cast(tmp, tf.int32)
+        indices = tf.reshape(indices, [-1])
+        return indices
 
 
 def get_output(last_state, vocab_size, activation=tf.nn.relu, initializer=None, scope=None):
