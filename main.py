f724fe526878111a758198ee7423e2cb81b1a7eb
Yannis Flet-Berliac <yannisfbe@gmail.com>
2017-02-08 16:40:14 +0100

    fix a bug that caused some words (layer 2) not being extracted

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('seed', 14, 'Random seed.')
+tf.app.flags.DEFINE_integer('seed', 67, 'Random seed.')
 tf.app.flags.DEFINE_string('dataset_path', 'datasets/processed/qa1_single-supporting-fact_1k.json', 'Dataset path.')
 tf.app.flags.DEFINE_string('model_dir', 'logs/', 'Model directory.')
 tf.app.flags.DEFINE_integer('examples_per_epoch', 1000, 'Number of examples per epoch.')
-tf.app.flags.DEFINE_integer('batch_size', 20, 'Batch size.')
+tf.app.flags.DEFINE_integer('batch_size', 40, 'Batch size.')
 tf.app.flags.DEFINE_integer('num_epochs', 200, 'Number of training epochs.')
 tf.app.flags.DEFINE_integer('embedding_size', 100, 'Embedding size.')
-tf.app.flags.DEFINE_integer('hidden_units', 1000, 'GRU hidden units.')
+tf.app.flags.DEFINE_integer('hidden_units', 100, 'GRU hidden units.')
 tf.app.flags.DEFINE_float('learning_rate', 1e-2, 'Base learning rate.')
 tf.app.flags.DEFINE_float('clip_gradients', 40.0, 'Clip the global norm of the gradients to this value.')
 tf.app.flags.DEFINE_integer('early_stopping_rounds', 1000, 'Number of epochs before early stopping.')
@@ -56,7 +56,7 @@ def main(_):
 
     config = tf.contrib.learn.RunConfig(
         tf_random_seed=FLAGS.seed,
-        save_summary_steps=20,
+        save_summary_steps=120,
         save_checkpoints_secs=120,
         keep_checkpoint_max=5,
         keep_checkpoint_every_n_hours=1,
@@ -90,7 +90,6 @@ def main(_):
         eval_input_fn,
         train_steps=None,
         eval_steps=None,
-        min_eval_frequency=1,
         eval_metrics=validation_metrics,
         train_monitors=validation_monitors,
         local_eval_frequency=1)
--- model/model.py
+++ model/model.py
@@ -19,29 +19,36 @@ def model_fn(features, targets, mode, params, scope=None):
     hidden_units = params['hidden_units']
     debug = params['debug']
 
-    story = features['story']  # ? * 1 * 311
+    story = features['story']  # [? * 1 * 311]
+    query = features['query']  # [? * 1 * 4]
 
     batch_size = tf.shape(story)[0]
+
     normal_initializer = tf.random_normal_initializer(stddev=0.1)
+    ones_initializer = tf.constant_initializer(1.0)
 
     with tf.variable_scope(scope, 'Mango', initializer=normal_initializer):
-        # Embeddings taking care of paddings
+        # Input
         embedding_params = tf.get_variable('embedding_params', [vocab_size, embedding_size])
         embedding_mask = tf.constant([0 if i == 0 else 1 for i in range(vocab_size)],
                                      dtype=tf.float32,
                                      shape=[vocab_size, 1])
-        embedding_params_masked = embedding_params * embedding_mask  # 38 * embedding_size
-        story_embedding = tf.nn.embedding_lookup(embedding_params_masked, story)  # ? * 1 * 311 * embedding_size
+        embedding_params_masked = embedding_params * embedding_mask  # [38 * embedding_size]
+        story_embedding = tf.nn.embedding_lookup(embedding_params_masked, story)  # [? * 1 * 311 * embedding_size]
+        query_embedding = tf.nn.embedding_lookup(embedding_params_masked, query)
 
         indices_word = get_word_indices(story, token_space)  # index(words) # TODO add the dots for the missing words
         indices_sentence = get_sentence_indices(story, token_sentence)  # index(sentences)
+        indices_word = tf.concat(0, [indices_word, indices_sentence])  # get all the words
+
+        embedded_input = tf.squeeze(story_embedding, [1])
+        encoded_query = tf.squeeze(get_input_encoding(query_embedding, ones_initializer, 'QueryEncoding'), [1])
 
         # Recurrence
-        story_length = get_story_char_length(story_embedding)
+        char_length = get_story_char_length(story_embedding)
         word_length = get_story_word_length(story, token_space, vocab_size, embedding_size)
         sentence_length = get_story_sentence_length(story, token_sentence, vocab_size, embedding_size)
 
-        embedded_input = tf.squeeze(story_embedding, [1])
         # TODO remove non-linearities between layers
         cell_1 = tf.nn.rnn_cell.GRUCell(hidden_units)
         cell_2 = tf.nn.rnn_cell.GRUCell(hidden_units)
@@ -51,32 +58,32 @@ def model_fn(features, targets, mode, params, scope=None):
         initial_state_3 = cell_3.zero_state(batch_size, tf.float32)
 
         with tf.variable_scope('cell_1'):
-            outputs_1, _ = tf.nn.dynamic_rnn(cell_1, embedded_input,  # ? * 311 * embedding_size
-                                             sequence_length=story_length,
+            outputs_1, _ = tf.nn.dynamic_rnn(cell_1, embedded_input,  # [? * 311 * embedding_size]
+                                             sequence_length=char_length,
                                              initial_state=initial_state_1)
 
-            ##GOOD##
-            aa = tf.gather_nd(outputs_1, indices_word)
-            #########
+            outputs_1_masked = tf.gather_nd(outputs_1, indices_word)
 
             input_rnn2 = []
             length = 0
-            zero_padding = tf.zeros([batch_size, max_story_word_length, embedding_size], dtype=aa.dtype)
+            zero_padding = tf.zeros([batch_size, max_story_word_length, embedding_size], dtype=outputs_1_masked.dtype)
 
             for i in xrange(batch_size_int):
                 length_padding = max_story_word_length - word_length[i] + 1
-                input_rnn2.append(tf.add(zero_padding[i], tf.concat(0, [tf.strided_slice(aa, [length, 0],
-                                                                           [length + word_length[i]-1, 100],
-                                                                           [1, 1]), tf.zeros([length_padding, embedding_size], dtype=aa.dtype)])))
+                input_rnn2.append(tf.add(zero_padding[i], tf.concat(0, [tf.strided_slice(outputs_1_masked, [length, 0],
+                                                                                         [length + word_length[i] - 1,
+                                                                                          100],
+                                                                                         [1, 1]),
+                                                                        tf.zeros([length_padding, embedding_size],
+                                                                                 dtype=outputs_1_masked.dtype)])))
                 length += word_length[i]
 
             input_rnn2 = tf.pack(input_rnn2)
 
-
         with tf.variable_scope('cell_2'):
-            outputs_2, last_state_2 = tf.nn.dynamic_rnn(cell_2, input_rnn2,
-                                                        sequence_length=word_length,
-                                                        initial_state=initial_state_2)
+            _, last_state_2 = tf.nn.dynamic_rnn(cell_2, input_rnn2,
+                                                sequence_length=word_length,
+                                                initial_state=initial_state_2)
 
             # TODO find a way to extract the sentence indices
             # bb = tf.gather_nd(outputs_2, indices_sentence)
@@ -88,7 +95,7 @@ def model_fn(features, targets, mode, params, scope=None):
 
         # Output
         # TODO make RNN for Output - "transfer learning from the encoder?"
-        output = get_output(last_state_2,
+        output = get_output(last_state_2, encoded_query,
                             vocab_size=vocab_size,
                             initializer=normal_initializer
                             )
@@ -99,21 +106,33 @@ def model_fn(features, targets, mode, params, scope=None):
         train_op = training_optimizer(loss, params, mode)
 
         if debug:
-            # tf.contrib.layers.summarize_tensor(offset, 'offset')
-            # tf.contrib.layers.summarize_tensor(word_flattened_indices, 'word_flattened_indices')
-            # tf.contrib.layers.summarize_tensor(embedded_input, 'embedded_input')
-            # tf.contrib.layers.summarize_tensor(flattened_output_1, 'flattened_output_1')
-            # tf.contrib.layers.summarize_tensor(selected_rows_word, 'selected_rows_word')
-            # tf.contrib.layers.summarize_tensor(input_rnn2, 'input_rnn2')
-            # tf.contrib.layers.summarize_tensor(indices_word, 'indices_word')
-            # tf.contrib.layers.summarize_tensor(story_length, 'story_length')
-            # tf.contrib.layers.summarize_tensor(last_state_3, 'last_state')
-            # tf.contrib.layers.summarize_tensor(output, 'output')
+            tf.contrib.layers.summarize_tensor(offset, 'offset')
+            tf.contrib.layers.summarize_tensor(word_flattened_indices, 'word_flattened_indices')
+            tf.contrib.layers.summarize_tensor(embedded_input, 'embedded_input')
+            tf.contrib.layers.summarize_tensor(flattened_output_1, 'flattened_output_1')
+            tf.contrib.layers.summarize_tensor(selected_rows_word, 'selected_rows_word')
+            tf.contrib.layers.summarize_tensor(input_rnn2, 'input_rnn2')
+            tf.contrib.layers.summarize_tensor(indices_word, 'indices_word')
+            tf.contrib.layers.summarize_tensor(char_length, 'char_length')
+            tf.contrib.layers.summarize_tensor(last_state_3, 'last_state')
+            tf.contrib.layers.summarize_tensor(output, 'output')
             tf.contrib.layers.summarize_variables()
 
             tf.add_check_numerics_ops()
 
         return prediction, loss, train_op
+
+
+def get_input_encoding(embedding, initializer=None, scope=None):
+    """
+    Implementation of a Position Encoding. The mask allows
+    the ordering of words in a sentence to affect the encoding.
+    """
+    with tf.variable_scope(scope, 'Encoding', initializer=initializer):
+        _, _, max_sentence_length, _ = embedding.get_shape().as_list()
+        positional_mask = tf.get_variable('positional_mask', [max_sentence_length, 1])
+        encoded_input = tf.reduce_sum(embedding * positional_mask, reduction_indices=[2])
+        return encoded_input
 
 
 def get_story_word_length(story, token, vocab_size, embedding_size, scope=None):
@@ -177,14 +196,25 @@ def get_word_indices(story, token_space, scope=None):
         return indices
 
 
-def get_output(last_state, vocab_size, activation=tf.nn.relu, initializer=None, scope=None):
+def get_output(last_state, encoded_query, vocab_size, activation=tf.nn.relu, initializer=None, scope=None):
     with tf.variable_scope(scope, 'Output', initializer=initializer):
         _, embedding_size = last_state.get_shape().as_list()
+
+        # Use the encoded_query to attend over memories (hidden states of dynamic last_state cell blocks)
+        attention = tf.reduce_sum(last_state * encoded_query, reduction_indices=[1])
+
+        # Subtract max for numerical stability (softmax is shift invariant)
+        attention_max = tf.reduce_max(attention, reduction_indices=[-1], keep_dims=True)
+        attention = tf.nn.softmax(attention - attention_max)
+        attention = tf.expand_dims(attention, 1)
+
+        # Weight memories by attention vectors
+        u = last_state * attention
 
         R = tf.get_variable('R', [embedding_size, vocab_size])
         H = tf.get_variable('H', [embedding_size, embedding_size])
 
-        y = tf.matmul(activation(tf.matmul(last_state, H)), R)
+        y = tf.matmul(activation(encoded_query + tf.matmul(u, H)), R)
         return y
 
 
